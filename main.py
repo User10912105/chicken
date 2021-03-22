f42686d36db1fc7654a270e23b966566d2bcf6a9
User10912105 <79892301+User10912105@users.noreply.github.com>
2021-03-22 22:06:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+class Animal():
+    def __init__(self,name):
+      self.name = name
+class chick(Animal):
+    def __init__(self,name):
+        super().__init__(self,name)
+n=str(input("請輸入名字"))
+print(n,'誕生了')
